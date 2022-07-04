b5d897a919a00fc597061c30575289b56627cb31
azrinurvani <azrinurvani@gmail.com>
2022-07-04 10:55:07 +0700

    - Second Commit

--- app/src/main/java/com/mobile/azri/movieappmvvm/MovieApplication.kt
+++ app/src/main/java/com/mobile/azri/movieappmvvm/MovieApplication.kt
@@ -2,6 +2,10 @@ package com.mobile.azri.movieappmvvm
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+/*Template Android App July 2022 using Coroutine
+Created by : Azri Nurvani
+* */
+
 
 //TODO 5 - Add MovieApplication extends application with annotation HiltAndroidApp
 @HiltAndroidApp
